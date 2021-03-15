ffc1d1f7323e27ee1906a8c24390a1ee21301c7f
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-15 15:22:36 -0700

    front end new cupcake form and functionality

--- static/index.js
+++ static/index.js
@@ -1,16 +1,35 @@
-cupcakesList = $("#cupcakes-list")
+$cupcakesList = $("#cupcakes-list")
+$cupcakeForm = $("#new-cupcake-form")
+$flavorInput = $("#flavor")
+$ratingInput = $("#rating")
+$sizeInput = $("#size")
+$imageInput = $("#image")
 
 async function start() {
-    console.log("test")
-    console.log(cupcakesList)
     let response = await axios.get("/api/cupcakes")
-
-    console.log(response)
-    cupcakes = response.data.cupcakes
-    console.log(cupcakes)
+    let {cupcakes} = response.data
     for (let cupcake of cupcakes) {
-        cupcakesList.append(`<li>${ cupcake.flavor }</li>`)
+        $cupcakesList.append(`<li>${ cupcake.flavor }</li>`) // helper function to add to DOM 
     }
 }
+
+$cupcakeForm.on('submit', async (e) => {
+    e.preventDefault();
+    //e.target fields from name attribute
+    let flavor = $flavorInput.val();
+    let rating = $ratingInput.val();
+    let size = $sizeInput.val();
+    let image = $imageInput.val();
+    let response = await axios.post('/api/cupcakes', {
+        flavor,
+        rating,
+        size,
+        image
+    })
+
+    let {flavor} = response.data.cupcake;
+    
+    $cupcakesList.append(`<li>${ flavor }</li>`)
+})
 
 start()
