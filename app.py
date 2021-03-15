7ed697facfcafc06f4b8805632597095521ca4ac
Christina Schweipert <c.schweipert@gmail.com>
2021-03-15 13:02:24 -0600

    add cupcake routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, jsonify, request
 
 # import requests
 
@@ -21,3 +21,42 @@ connect_db(app)
 db.create_all()
 
 toolbar = DebugToolbarExtension(app)
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """Show all cupcakes"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def show_cupcake(cupcake_id):
+    """Show ONE cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+    return jsonify(cupcake=serialized)
+
+
+@app.route("/api/cupcakes", methods=["POST"])
+def add_cupcake():
+    """Add a cupcake"""
+
+    data = request.json
+
+    cupcake = Cupcake(
+        flavor=data["flavor"],
+        size=data["size"],
+        rating=data["rating"],
+        image=data.get("image")
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+    print(cupcake.serialize())
+
+    return (jsonify(cupcake=cupcake.serialize()), 201)
+
