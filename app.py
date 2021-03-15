8813e885b6deff25dec5a94a43a63158484eeefd
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-15 11:15:49 -0700

    seeded database, cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, redirect, flash
+
+# import requests
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """ Cupcake table """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(50), nullable=False)
+    size = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake")
+
+
