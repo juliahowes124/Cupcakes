d7c3513765159088463a6d610a3cdf285261b6b8
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-04-02 12:53:48 -0700

    changed UI to search on input change rather than submit click

--- static/index.js
+++ static/index.js
@@ -8,45 +8,43 @@ $searchForm = $("#search-form")
 $searchTerm = $("#search-term")
 
 async function start() {
-    let response = await axios.get("/api/cupcakes")
-    let { cupcakes } = response.data
-    for (let cupcake of cupcakes) {
-        $cupcakesList.append(`<li>${cupcake.flavor}</li>`) // helper function to add to DOM 
-    }
+	let response = await axios.get("/api/cupcakes")
+	let { cupcakes } = response.data
+	for (let cupcake of cupcakes) {
+		$cupcakesList.append(`<li>${cupcake.flavor}</li>`) // helper function to add to DOM 
+	}
 }
 
 $cupcakeForm.on('submit', async (e) => {
-    e.preventDefault();
-    //e.target fields from name attribute
-    let flavor = $flavorInput.val();
-    let rating = $ratingInput.val();
-    let size = $sizeInput.val();
-    let image = $imageInput.val();
-    let response = await axios.post('/api/cupcakes', {
-        flavor,
-        rating,
-        size,
-        image
-    })
+	e.preventDefault();
+	//e.target fields from name attribute
+	let flavor = $flavorInput.val();
+	let rating = $ratingInput.val();
+	let size = $sizeInput.val();
+	let image = $imageInput.val();
+	let response = await axios.post('/api/cupcakes', {
+		flavor,
+		rating,
+		size,
+		image
+	})
 
-    let { cupcakeFlavor } = response.data.cupcake;
+	let { cupcakeFlavor } = response.data.cupcake;
 
-    $cupcakesList.append(`<li>${cupcakeFlavor}</li>`)
+	$cupcakesList.append(`<li>${cupcakeFlavor}</li>`)
 })
 
-$searchForm.on("submit", async (e) => {
-    e.preventDefault();
-
-    let searchTerm = $searchTerm.val();
-
-    let response = await axios.get('/api/cupcakes', { params: { searchTerm } });
-
-    $cupcakesList.empty()
-
-    let { cupcakes } = response.data
-    for (let cupcake of cupcakes) {
-        $cupcakesList.append(`<li>${cupcake.flavor}</li>`) // helper function to add to DOM 
-    }
+$searchTerm.on("input", async (e) => {
+	let searchTerm = $searchTerm.val();
+	let response = await axios.get('/api/cupcakes', { params: { searchTerm } });
+	addCupcakesToDom(response.data.cupcakes);
 })
 
+
+function addCupcakesToDom(cupcakes) {
+	$cupcakesList.empty();
+	for (let cupcake of cupcakes) {
+		$cupcakesList.append(`<li>${cupcake.flavor}</li>`) // helper function to add to DOM 
+	}
+}
 start()
